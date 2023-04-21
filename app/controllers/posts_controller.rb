88141058307c32918c08316101900bc17b5befed
Ted Leahy <tleahy@uplandsoftware.com>
2023-04-22 00:39:38 +0100

    Allow users to create and view a post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
